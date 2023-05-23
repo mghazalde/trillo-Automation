b3c98407bc60e785bbb8dbfd40f5450f586330ba
mghazalde <102529622+mghazalde@users.noreply.github.com>
2023-05-23 10:30:22 +0200

    Add files via upload

--- src/main/java/apis/BoardPage.java
+++ src/main/java/apis/BoardPage.java
@@ -1,55 +1,55 @@
 package apis;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-	
 public class BoardPage {
 
 	private static String boardId;
-    public static String getBoardId() {
-        return boardId;
-    }
-	private String  responseBody;
-	
+
+	public static String getBoardId() {
+		return boardId;
+	}
+
+	private String responseBody;
+
 	public String getResponseBody() {
-        return responseBody;
-    }
-    private String expectedResponseBody;
-    public String getExpectedresBody() {
-        return expectedResponseBody;
-    }
-   
-    private int actualStatusCode;
-    public int getStatusCode() {
-    	return actualStatusCode;
-    }
-    
-    String url = System.getProperty("baseURI");
+		return responseBody;
+	}
 
-    String token = System.getProperty("token");
-    String key = System.getProperty("key");
-    String requestBodyBoard = System.getProperty("requestBodyBoard");
-    public void createBoard()
-    
-    {
-    	
-    	//apiData = new SHAFT.TestData.JSON("testData.json");
-        RestAssured.baseURI = url;
-        Response response = RestAssured.given()
-                .header("Content-Type", "application/json")
-                .queryParam("key", key)
-                .queryParam("token", token)
-                .body(requestBodyBoard)
-                .post("/boards");
+	private String expectedResponseBody;
 
-        // Validate the status code
-        this.actualStatusCode = response.getStatusCode();
+	public String getExpectedresBody() {
+		return expectedResponseBody;
+	}
 
-        // Validate the response body
-         this.expectedResponseBody = System.getProperty("expectedResponseBoard");
-         this.responseBody = response.getBody().asString();
+	private int actualStatusCode;
 
-        // Extract the board ID from the response
-        this.boardId = response.jsonPath().getString("id");
-    }
+	public int getStatusCode() {
+		return actualStatusCode;
+	}
+
+	String url = System.getProperty("baseURI");
+	String token = System.getProperty("token");
+	String key = System.getProperty("key");
+	String requestBodyBoard = System.getProperty("requestBodyBoard");
+
+	public void createBoard()
+
+	{
+
+		RestAssured.baseURI = url;
+		Response response = RestAssured.given().header("Content-Type", "application/json").queryParam("key", key)
+				.queryParam("token", token).body(requestBodyBoard).post("/boards");
+
+		// Validate the status code
+		this.actualStatusCode = response.getStatusCode();
+
+		// Validate the response body
+		this.expectedResponseBody = System.getProperty("expectedResponseBoard");
+		this.responseBody = response.getBody().asString();
+
+		// Extract the board ID from the response
+		this.boardId = response.jsonPath().getString("id");
+	}
 }
--- src/main/java/apis/CardPage.java
+++ src/main/java/apis/CardPage.java
@@ -9,68 +9,59 @@ import io.restassured.response.Response;
 public class CardPage {
 	private BoardPage boardPage;
 	private ListPage listPage;
-	 
-    String url = System.getProperty("baseURI");
 
-    String token = System.getProperty("token");
-    String key = System.getProperty("key");
-    String requestBodyCard = System.getProperty("requestBodyCard");
-    
-    
-    private int actualStatusCode;
-    public int getStatusCode() {
-    	return actualStatusCode;
-    }
-    
+	String url = System.getProperty("baseURI");
+
+	String token = System.getProperty("token");
+	String key = System.getProperty("key");
+	String requestBodyCard = System.getProperty("requestBodyCard");
+
+	private int actualStatusCode;
+
+	public int getStatusCode() {
+		return actualStatusCode;
+	}
+
 	private String responseBody;
-	
+
 	public String getResponseBody() {
-        return responseBody;
-    }
-    
-   private String expectedResponseBody;
-   public String getExpectedresBody() {
-       return expectedResponseBody;
-    	    
-    }
-   private static String cardId;
-   public static String getCardId() {
-        return cardId;
-    }
+		return responseBody;
+	}
 
+	private String expectedResponseBody;
 
+	public String getExpectedresBody() {
+		return expectedResponseBody;
 
+	}
 
-    public void CreateCard() {
-    	ListPage listPage = new ListPage();
-   	    String listId = listPage.getListId();
-     	RestAssured.baseURI = url;
-   
+	private static String cardId;
 
-        Response response = RestAssured.given()
-                .queryParam("idList", listId)
-                .queryParam("key", key)
-                .queryParam("token", token)
-                .contentType(ContentType.JSON)
-                .body(requestBodyCard)
-                .post("/cards");
+	public static String getCardId() {
+		return cardId;
+	}
 
-        int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 200, "Expected status code 200, but found " + statusCode);
+	public void CreateCard() {
+		ListPage listPage = new ListPage();
+		String listId = listPage.getListId();
+		RestAssured.baseURI = url;
 
-        String cardId = response.jsonPath().getString("id");
-        Assert.assertNotNull(cardId, "Card ID is null");
+		Response response = RestAssured.given().queryParam("idList", listId).queryParam("key", key)
+				.queryParam("token", token).contentType(ContentType.JSON).body(requestBodyCard).post("/cards");
 
-        System.out.println("Created Card ID: " + cardId);
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 200, "Expected status code 200, but found " + statusCode);
 
-  
-        
-        this.expectedResponseBody = System.getProperty("expectedResponseCard");
-        this.responseBody = response.getBody().asString();
-        this.actualStatusCode = response.getStatusCode();
-  
-        // Extract the list ID from the response
-        this.cardId = response.jsonPath().getString("id");
-    }
+		String cardId = response.jsonPath().getString("id");
+		Assert.assertNotNull(cardId, "Card ID is null");
+
+		System.out.println("Created Card ID: " + cardId);
+
+		this.expectedResponseBody = System.getProperty("expectedResponseCard");
+		this.responseBody = response.getBody().asString();
+		this.actualStatusCode = response.getStatusCode();
+
+		// Extract the list ID from the response
+		this.cardId = response.jsonPath().getString("id");
+	}
 }
-
