c2802be9355d04602846f6918bc9d736fe05dbd7
mghazalde <102529622+mghazalde@users.noreply.github.com>
2023-05-23 10:32:55 +0200

    Add files via upload

--- src/test/java/tests/ApiTest.java
+++ src/test/java/tests/ApiTest.java
@@ -15,21 +15,21 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class ApiTest {
-	private String key;
-	private String token;
 	private BoardPage boardPage;
 	private ListPage listPage;
 	private CardPage cardPage;
+
+	String url = System.getProperty("baseURI");
+	String token = System.getProperty("token");
+	String key = System.getProperty("key");
 
 	@BeforeClass
 	public void setUp() {
 
 		// Deleting any created boards before test
 
-		RestAssured.baseURI = "https://api.trello.com/1";
-		Response response = RestAssured.given().param("fields", "name,url")
-				.param("key", "baf4ed3a72162192e77c6771ba0e9e77")
-				.param("token", "ATTAbcd413aa498bac832fa1eb4a27de5395735eb8f3c9b18827244ba1273828932d8522F24A")
+		RestAssured.baseURI = url;
+		Response response = RestAssured.given().param("fields", "name,url").param("key", key).param("token", token)
 				.get("/members/me/boards");
 		// Parse the response using JsonPath
 		JsonPath jsonPath = response.jsonPath();
@@ -39,11 +39,9 @@ public class ApiTest {
 
 		// Iterate through the board IDs and send DELETE requests for each ID
 		for (String boardId : boardIds) {
-			Response deleteResponse = RestAssured.given().queryParam("key", "baf4ed3a72162192e77c6771ba0e9e77")
-					.queryParam("token", "ATTAbcd413aa498bac832fa1eb4a27de5395735eb8f3c9b18827244ba1273828932d8522F24A")
+			Response deleteResponse = RestAssured.given().queryParam("key", key).queryParam("token", token)
 					.delete("/boards/" + boardId);
 		}
-
 
 		// Instantiate the page objects
 		boardPage = new BoardPage();
@@ -74,7 +72,6 @@ public class ApiTest {
 	@Test(priority = 3)
 
 	public void validateBoardName() {
-		// boardPage.createBoard();
 		String responseBody = boardPage.getResponseBody();
 		String expectedResponseBody = boardPage.getExpectedresBody();
 		Assert.assertTrue(responseBody.contains(expectedResponseBody), "Expected boardName in response body");
@@ -142,6 +139,6 @@ public class ApiTest {
 
 	@AfterClass
 	public void tearDown() {
-		
+
 	}
 }
